51fe79bc2724e5610c6311614ad1fd359fc48fb6
michaeldsviana <michaelpf2013@gmail.com>
2026-08-30 19:04:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   base: './',
-
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false
-  },
-
-  server: {
-    host: true,
-    port: 5173
+    sourcemap: false,
+    target: ['es2019', 'safari14', 'chrome80', 'firefox78'],
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          charts: ['recharts'],
+          supabase: ['@supabase/supabase-js']
+        }
+      }
+    }
   }
 })
